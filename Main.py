81643d8268d2bf10becb688958dd6216c05ba8b1
toomastahves <toomastahves@hotmail.com>
2017-06-25 14:09:52 +0300

    added matrices

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import Vectors
+# import Vectors
+# print(Vectors.distance([1,1], [2,2]))
 
-print(Vectors.distance([1,1], [2,2]))
+import Matrices
+m = Matrices.make_matrix(5, 5, Matrices.is_diagonal)
+print(m)
